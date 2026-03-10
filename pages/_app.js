fb2ef5af09949d1dc3a75351239c9ff66dbec919
GoThereNow <support@gotherenow.app>
2026-03-10 10:18:10 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,18 @@
 import '../styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="travelpayouts-site-verification" content="<script data-noptimize="1" data-cfasync="false" data-wpfc-render="false">
-  (function () {
-      var script = document.createElement("script");
-      script.async = 1;
-      script.src = 'https://emrldco.com/NTA2NDE5.js?t=506419';
-      document.head.appendChild(script);
-  })();
-</script>" />
+        <title>GoThereNow</title>
       </Head>
+      <Script
+        src="https://emrldco.com/NTA2NDE5.js?t=506419"
+        strategy="afterInteractive"
+      />
       <Component {...pageProps} />
     </>
   )

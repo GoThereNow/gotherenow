f898c72540e8eb856e2e5d5a569c8e656f711c20
GoThereNow <support@gotherenow.app>
2026-03-10 14:30:11 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://api.mapbox.com https://emrldco.com; worker-src blob:; child-src blob:;",
+          },
+        ],
+      },
+    ]
+  },
 }
-
 module.exports = nextConfig

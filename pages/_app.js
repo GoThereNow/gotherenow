8d8119a55930902478d605bc28eb213972dba618
GoThereNow <support@gotherenow.app>
2026-03-10 14:12:54 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
-import 'mapbox-gl/dist/mapbox-gl.css'
 import Head from 'next/head'
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('mapbox-gl/dist/mapbox-gl.css')
+  }, [])
+
   return (
     <>
       <Head>

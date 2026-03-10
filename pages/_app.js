e3a49e14cdd8b554150c9115c1466727a0d4bb64
GoThereNow <support@gotherenow.app>
2026-03-10 14:14:27 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,5 @@
 import '../styles/globals.css'
-import Head from 'next/head'
-import Script from 'next/script'
-import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    import('mapbox-gl/dist/mapbox-gl.css')
-  }, [])
-
-  return (
-    <>
-      <Head>
-        <title>GoThereNow</title>
-      </Head>
-      <Script
-        src="https://emrldco.com/NTA2NDE5.js?t=506419"
-        strategy="afterInteractive"
-      />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
